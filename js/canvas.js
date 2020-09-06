fb64021a3184d7f80ad52de943922ba6fcc7c802
Savelen <konorsavelen@gmail.com>
2020-09-07 01:41:08 +0300

    Need to fix bug

--- js/canvas.js
+++ js/canvas.js
@@ -1,10 +1,3 @@
-//создание canvas и определение его размера
-let canvas = document.querySelector('#graph');
-// canvas.width = window.innerWidth - 46;
-// canvas.height = window.innerHeight / 100 * 80 - 32;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-//----------------------------------------------------------------------
 //определение основных функций canvas в обьекте
 function dataCanvas(can, arrel = new Map(), activ = new Map()) {
 	return {
@@ -15,10 +8,11 @@ function dataCanvas(can, arrel = new Map(), activ = new Map()) {
 		activeName: false,
 		activeId: '',
 		check: true,
+		// отрисовка
+		lock: 60, // максимальное колличество отрисовок в 1 секунду
 		time: Date.now(),
 		timeCount: 0,
-		fps: 0,
-		lock: 60,
+		fps: 0, // показатель отрисовок в 1 секунду
 		lockCount: 0,
 		// рисуем всё или 1 обьект (по имени или обЪекту) используя на drawEL
 		draw: function (element = false) {
@@ -64,8 +58,14 @@ function dataCanvas(can, arrel = new Map(), activ = new Map()) {
 		// ресует 1 обьект (по обЪекту)
 		drawEl: function (el) {
 			if (el.type == 'l') {
-				this.ctx.fillStyle = el.color;
-				this.ctx.fillRect(el.x, el.y, el.w, el.h);
+				if (el.fill) {
+					this.ctx.fillStyle = el.color;
+					this.ctx.fillRect(el.x, el.y, el.w, el.h);
+				}
+				else {
+					this.ctx.strokeStyle = el.color;
+					this.ctx.strokeRect(el.x, el.y, el.w, el.h);
+				}
 			}
 			else if (el.type == 'o') {
 				let conf = el.conf;
@@ -92,6 +92,7 @@ function dataCanvas(can, arrel = new Map(), activ = new Map()) {
 				this.ctx.closePath();
 				if (conf.type == "f") this.ctx.fill();
 				else this.ctx.stroke();
+
 			}
 			else if (el.type = 'c') {
 				el.arrObj.forEach((e) => {
@@ -100,31 +101,32 @@ function dataCanvas(can, arrel = new Map(), activ = new Map()) {
 			}
 		},
 		//рисует линию и добавляет в список элементов с тмпом "l"
-		drawLine: function (el = false, x = 0, y = 0, w = 0, h = 0, color = 'black',) {
+		drawLine: function (el = false, x = 0, y = 0, w = 0, h = 0, color = 'black', fill = true) {
 			let name = this.makeid(32);
 			let point = [];
+			color = color ? color : "black";
 			if (!el) {
 				point.push({ map: this.pointGet(x, x + w, y, y, name) });
 				point.push({ map: this.pointGet(x, x + w, y + h, y + h, name) });
 				point.push({ map: this.pointGet(x, x, y, y + h, name) });
 				point.push({ map: this.pointGet(x + w, x + w, y, y + h, name) });
-				this.elements.set(name, { name: name, type: 'l', y: y, x: x, w: w, h: h, color: color, point: point });
+				this.elements.set(name, { name: name, type: 'l', y: y, x: x, w: w, h: h, color: color, fill: fill, point: point });
 			}
 			else if (!el.hasOwnProperty("name")) {
-				point.push({ map: this.pointGet(el.x, el.x + el.w, el.y, el.y, el.name) });
-				point.push({ map: this.pointGet(el.x, el.x + el.w, el.y + el.h, el.y + el.h, el.name) });
-				point.push({ map: this.pointGet(el.x, el.x, el.y, el.y + el.h, el.name) });
-				point.push({ map: this.pointGet(el.x + el.w, x + el.w, el.y, el.y + el.h, el.name) });
-				this.elements.set(name, { name: name, type: 'l', y: el.y, x: el.x, w: el.w, h: el.h, color: el.color, point: point });
+				point.push({ map: this.pointGet(el.x, el.x + el.w, el.y, el.y, name) });
+				point.push({ map: this.pointGet(el.x, el.x + el.w, el.y + el.h, el.y + el.h, name) });
+				point.push({ map: this.pointGet(el.x, el.x, el.y, el.y + el.h, name) });
+				point.push({ map: this.pointGet(el.x + el.w, x + el.w, el.y, el.y + el.h, name) });
+				this.elements.set(name, { name: name, type: 'l', y: el.y, x: el.x, w: el.w, h: el.h, color: el.color, fill: el.fill, point: point });
 			}
 			else if (el.hasOwnProperty("name")) {
 				point.push({ map: this.pointGet(el.x, el.x + el.w, el.y, el.y, el.name) });
 				point.push({ map: this.pointGet(el.x, el.x + el.w, el.y + el.h, el.y + el.h, el.name) });
 				point.push({ map: this.pointGet(el.x, el.x, el.y, el.y + el.h, el.name) });
 				point.push({ map: this.pointGet(el.x + el.w, x + el.w, el.y, el.y + el.h, el.name) });
-				this.elements.set(el.name, { name: el.name, type: 'l', y: el.y, x: el.x, w: el.w, h: el.h, color: el.color, point: point });
+				this.elements.set(el.name, { name: el.name, type: 'l', y: el.y, x: el.x, w: el.w, h: el.h, color: el.color, fill: el.fill, point: point });
 			}
-			this.draw();
+			this.draw(this.elements.get(name));
 		},
 		//рисует обьект по точкам и добавляет в список элементов с тмпом "o"
 		drawObj: function (obj, conf = { default: true, type: 's', color: "black", widht: 1, name: false, cap: 'butt', join: 'round', limit: 1, dash: [], dashOffset: 0 }) {
@@ -140,7 +142,7 @@ function dataCanvas(can, arrel = new Map(), activ = new Map()) {
 				pointArr.push({ x1: p1[0], y1: p1[1], x2: p2[0], y2: p2[1], map: map });
 			}
 			this.elements.set(conf.name, { name: conf.name, type: 'o', point: pointArr, conf: conf });
-			this.draw();
+			this.draw(this.elements.get(conf.name));
 		},
 		//генерация id
 		makeid: function (length) {
@@ -363,7 +365,7 @@ function dataCanvas(can, arrel = new Map(), activ = new Map()) {
 			if (update) this.draw();
 		},
 		// обьелиняет элементы в один
-		combEl: function (name = false, objArr, genObj = false) {
+		combEl: function (objArr, genObj = false, name = false) {
 			let result = {};
 			if (name) result.name = name;
 			else result.name = this.makeid(32);
@@ -396,91 +398,3 @@ function dataCanvas(can, arrel = new Map(), activ = new Map()) {
 		}
 	}
 }
-let can = dataCanvas(canvas);
-// can.drawLine(false, 0, 0, 10, 10);
-// can.drawLine(false, 10, 20, 30, 5, "red");
-// can.drawLine(false, 20, 40, 30, 5);
-// can.drawLine(false, 30, 60, 30, 5);
-// can.drawLine(false, 40, 80, 30, 5);
-// can.drawLine(false, 50, 100, 30, 5);
-// can.drawLine(false, 60, 120, 30, 5);
-// can.drawLine(false, 270, 240, 30, 5);
-
-// can.drawLine(false, 270, 240, 5, 50);
-
-can.drawObj([[100, 200], [400, 200], [400, 400], [280, 400], [280, 250], [220, 250], [220, 400], [100, 400]], { type: 's', widht: 1, name: "Стены", color: "#663" });
-can.drawObj([[310, 250], [340, 250], [340, 285], [340, 250], [370, 250], [370, 285], [340, 285], [370, 285], [370, 320], [340, 320], [340, 285], [340, 320], [310, 320], [310, 285], [340, 285], [310, 285]], { color: "blue", name: 'Окно 1' });
-can.drawObj([[130, 250], [160, 250], [160, 285], [160, 250], [190, 250], [190, 285], [160, 285], [190, 285], [190, 320], [160, 320], [160, 285], [160, 320], [130, 320], [130, 285], [160, 285], [130, 285]], { color: "blue", name: 'Окно 2' });
-can.drawObj([[100, 200], [400, 200], [250, 50]], { name: "Крыша", color: 'gray' });
-can.drawObj([[220, 250], [280, 250], [280, 400], [220, 400]], { name: "Дверь", color: '#531' });
-
-can.drawObj([[232, 327], [232, 320], [225, 320], [225, 327]], { name: "Дверная ручка", color: '#531' });
-can.drawObj([[1300, 300], [1000, 300], [1000, 500]], { name: 'прямоугольник' });
-can.drawObj([[600, 100], [800, 100], [700, 200], [600, 300], [800, 300]], { name: "time", color: "aqua" });
-can.eventGo();
-
-// обьединяем элементы
-can.combEl("Дом", ["Стены", "Крыша", "Окно 1", "Окно 2", "Дверь", "Стены", "Дверная ручка"], ["Стены", "Крыша"]);
-// делаем элементы интерактивными
-can.activeEl("Дом");
-can.activeEl('time');
-// анимация
-can.roundDash("прямоугольник", 30, 100, [10, 15, 5]);
-
-
-// делает все элементы активными
-// can.elements.forEach((e) => {
-// 	can.activeEl(e.name);
-// });
-
-// can.remove('time');
-
-// for (let i = 0, j = 0; i < 1300; i++, (j < 500) ? j += 2 : j = 0) {
-// 	setTimeout(() => {
-// 		can.drawObj([[i + 0, i/2], [i, i/2 + 0], [i, 400]], { color: "red" });
-// 	}, 10);
-// 	setTimeout(() => {
-// 		can.drawObj([[i, i/2 + 0], [i + 0, i/2], [i, 400]], { co2or: "green" });
-// 	}, 20);
-// }
-
-
-/*
-------------------------------------------------------------------------------------------------------------------
-let add = 5;
-let wName = (this.elements.has(winName)) ? this.elements.get(winName) : { type: 'c' };
-let lName = this.elements.get(this.activeName);
-if (lName) {
-	if (lName.type == "l") {
-		lName.x += (lName.x > 0) ? add / 2 : 0;
-		lName.y += (lName.y > 0) ? add / 2 : 0;
-		lName.h -= add;
-		lName.w -= add;
-		this.elements.set(this.activeName, lName);
-		this.draw();
-	}
-	else if (lName.type == "o") this.roundDash(this.activeName, false);
-}
-if (wName.type == "l") {
-	wName.x -= (wName.x > 0) ? add / 2 : 0;
-	wName.y -= (wName.y > 0) ? add / 2 : 0;
-	wName.h += add;
-	wName.w += add;
-	this.elements.set(this.activeName, wName);
-	this.draw();
-}
-else if (wName.type == "o") this.roundDash(this.activeName);
-//----------------------
-let nName = this.elements.get(this.activeName);
-if (nName) {
-	if (nName.type == "l") {
-		nName.x += (nName.x > 0) ? add / 2 : 0;
-		nName.y += (nName.y > 0) ? add / 2 : 0;
-		nName.h -= add;
-		nName.w -= add;
-		this.elements.set(this.activeName, nName);
-		this.draw();
-	}
-	else if (nName.type == "o") this.roundDash(this.activeName, false);
-}
-*/
